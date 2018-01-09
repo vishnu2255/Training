d4cfad06923d3666b09e908fd27e24cefbf71c78
vishnu2255 <vishnuvkb@hotmail.com>
2018-01-09 09:08:37 -0500

    frsttest

---
+++ New folder/T/src/testPackageTr/State.java
@@ -0,0 +1,20 @@
+package testPackageTr;
+
+public class State {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		for(int i=0;i<5;i++)
+		{
+			
+			if(i==3)
+			{
+				continue;
+			}
+			System.out.println(i);
+		}
+	}
+
+}
