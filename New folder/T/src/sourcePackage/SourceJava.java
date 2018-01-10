7529314e8493d1ec5b67002ea25405d983998b18
vishnu2255 <vishnuvkb@hotmail.com>
2018-01-10 15:46:13 -0500

    test

--- New folder/T/src/sourcePackage/SourceJava.java
+++ New folder/T/src/sourcePackage/SourceJava.java
@@ -1,11 +1,13 @@
 package sourcePackage;
 
-public class SourceJava {
+public  class SourceJava {
 public int pub =12;
 private int pri=13;
 protected int prot =32;
 int def=25;
 
-
+public int test(int a) {
+	return 0;
+}
 
 }
---
+++ New folder/T/src/testPackageTr/Triangles.java
@@ -0,0 +1,58 @@
+package testPackageTr;
+
+
+
+class Triangles {
+
+	
+	int base,height,s1,s2,s3;
+	
+	public Triangles(int base,int height)
+	{
+		this.base=base;
+		this.height=height;
+	}
+	
+	public Triangles(int s1,int s2,int s3)
+	{
+		this.s1=s1;
+		this.s2=s2;
+		this.s3=s3;
+	}
+	
+	public double getArea(int b,int h)
+	{
+		System.out.println("2 args");
+		return (0.5*b*h);
+	}
+	public int getArea(int a,int b,int c)
+	{
+		System.out.println("3 args");
+		return (a*b*c);
+	}
+	
+	@Override
+	public int hashCode()
+	{
+		return 100;
+		
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Triangles tr1= new Triangles(1,2,3);
+		tr1.getArea(1,2,3);
+		
+		Triangles tr2= new Triangles(1,2,3);
+		tr2.getArea(2,3);
+		
+		
+		System.out.println(tr1.toString());
+		
+		System.out.println(tr1.getClass().getName());
+		
+		tr2=null;
+		System.gc();
+	}
+
+}
